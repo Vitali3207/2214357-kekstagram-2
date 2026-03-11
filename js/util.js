023b2cbe8833d2fa4c43497b12ec68fac1662c25
Vitali3207 <makar3447@gmail.com>
2026-03-11 20:25:18 +0300

    Пятое ДЗ

---
+++ js/util.js
@@ -0,0 +1,10 @@
+const getRandomInteger = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+export { getRandomInteger, getRandomArrayElement };
